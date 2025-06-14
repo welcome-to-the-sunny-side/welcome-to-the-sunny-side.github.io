9d7f4621e1401de665f499a0fc52b66ad733d840
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2025-06-14 13:50:01 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://welcome-to-the-sunny-side.github.io',
   output: 'static',
   integrations: [svelte()],
 

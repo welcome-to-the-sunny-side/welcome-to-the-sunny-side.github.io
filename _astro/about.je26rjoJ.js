6bd6c5e1d1c48f864f4985596b6a50a5d01c0d57
nonadhocproblems <nonadhocproblems@gmail.com>
2025-11-27 04:27:11 +0530

    Updates

--- _astro/about.je26rjoJ.js
+++ _astro/about.je26rjoJ.js
@@ -6,4 +6,4 @@ the selection of problems here has very little to do with their quality or elega
 queue:
 - https://codeforces.com/contest/2066/problem/D2
 - https://codeforces.com/problemset/problem/2164/F2
-`;export{e as default};
+- https://codeforces.com/contest/2059/problem/E2`;export{e as default};

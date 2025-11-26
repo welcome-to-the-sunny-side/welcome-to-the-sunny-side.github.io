59a7c3833bee42f95b335bb64b4b192285a75fe7
nonadhocproblems <nonadhocproblems@gmail.com>
2025-11-26 19:03:40 +0530

    Updates

---
+++ _astro/ContentPane.BfSFKwjO.js
@@ -0,0 +1,1 @@
+import{C as i}from"./ContentPane.4fZVd4b5.js";import"./router.w6Jd3Pfz.js";import"./template.CUcBOH1Q.js";import"./render.DWYK8PyM.js";export{i as default};
---
+++ _astro/about.CyodrVWB.js
@@ -0,0 +1,9 @@
+const e=`solutions to cp problems
+
+the selection of problems here has very little to do with their quality or elegance, and everything to do with me wanting to write something down on a whim
+
+
+queue:
+- https://codeforces.com/contest/2066/problem/D2
+- https://codeforces.com/problemset/problem/2164/F2
+`;export{e as default};

c347c0713fcfaf01f6f282a032263120caa6be8a
nonadhocproblems <nonadhocproblems@gmail.com>
2026-02-27 13:07:00 +0530

    Updates

--- _astro/list.d-GgCsqz.js
+++ _astro/list.d-GgCsqz.js
@@ -1,4 +1,5 @@
 const e=`| Book   | Author |Finish Date | First Time? |
 |:-------|:----:|:-----:|:-----:|
 | Flowers for Algernon | Daniel Keyes | 2026-02-09 |  Y |
-| Permutation City | Greg Egan | 2026-02-19 | Y |`;export{e as default};
+| Permutation City | Greg Egan | 2026-02-19 | Y |
+| Schild's Ladder | Greg Egan | 2026-02-26 | Y |`;export{e as default};

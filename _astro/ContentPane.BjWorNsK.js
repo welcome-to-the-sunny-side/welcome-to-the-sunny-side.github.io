f7565be72bc2aea6e54ef1a595fae52b02ac9686
nonadhocproblems <nonadhocproblems@gmail.com>
2026-03-20 09:31:36 +0530

    Updates

---
+++ _astro/ContentPane.BjWorNsK.js
@@ -0,0 +1,1 @@
+import{C as i}from"./ContentPane.B4zXQbFY.js";import"./router.BAiMS5lh.js";import"./template.CUcBOH1Q.js";import"./render.DWYK8PyM.js";export{i as default};
---
+++ _astro/about.DPqIZzlc.js
@@ -0,0 +1,8 @@
+const t=`---
+title: About
+---
+
+- codeforces: https://codeforces.com/profile/DNR
+- github: https://github.com/welcome-to-the-sunny-side/
+- youtube: https://www.youtube.com/@welcome-to-the-sunny-side
+`;export{t as default};

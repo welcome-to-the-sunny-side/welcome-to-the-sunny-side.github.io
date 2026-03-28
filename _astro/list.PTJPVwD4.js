6f7fceb24a2ea6037c02016d897dcbdfffdd74f9
nonadhocproblems <nonadhocproblems@gmail.com>
2026-03-28 20:07:19 +0530

    Updates

--- _astro/list.PTJPVwD4.js
+++ _astro/list.PTJPVwD4.js
@@ -2,4 +2,6 @@ const e=`| Book   | Author |Finish Date | First Time? |
 |:-------|:----:|:-----:|:-----:|
 | Flowers for Algernon | Daniel Keyes | 2026-02-09 |  Y |
 | Permutation City | Greg Egan | 2026-02-19 | Y |
-| Schild's Ladder | Greg Egan | 2026-02-26 | Y |`;export{e as default};
+| Schild's Ladder | Greg Egan | 2026-02-26 | Y |
+| The Man Who Was Thursday | G.K. Chesterton | 2026-03-28 | Y |
+`;export{e as default};

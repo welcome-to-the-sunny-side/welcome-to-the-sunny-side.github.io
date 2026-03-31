17ad84c178448bee98b4a2a93f3b0fa7189f64e4
nonadhocproblems <nonadhocproblems@gmail.com>
2026-04-01 01:07:46 +0530

    Updates

--- _astro/list.DWmS-qCC.js
+++ _astro/list.DWmS-qCC.js
@@ -1,7 +1,7 @@
-const e=`| Book   | Author |Finish Date | First Time? |
+const n=`| Book   | Author |Finish Date | First Time? |
 |:-------|:----:|:-----:|:-----:|
 | Flowers for Algernon | Daniel Keyes | 2026-02-09 |  Y |
 | Permutation City | Greg Egan | 2026-02-19 | Y |
 | Schild's Ladder | Greg Egan | 2026-02-26 | Y |
 | The Man Who Was Thursday | G.K. Chesterton | 2026-03-28 | Y |
-`;export{e as default};
+| Hunger | Knut Hamsun | 2026-03-31 | Y |`;export{n as default};

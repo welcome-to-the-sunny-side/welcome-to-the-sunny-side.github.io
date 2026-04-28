20e3f398a75ba70feda8411bf88c2f4964d3d23a
nonadhocproblems <nonadhocproblems@gmail.com>
2026-04-28 17:58:35 +0530

    Updates

---
+++ _astro/list.D_UafaCU.js
@@ -0,0 +1,10 @@
+const n=`| Book | Author | Finish Date | Rating (/5) | First Time? |
+|:-------|:----:|:-----:|:-----:| :-----:|
+| Flowers for Algernon | Daniel Keyes | 2026-02-09 | 0.5 | Y |
+| Permutation City | Greg Egan | 2026-02-19 | 4 | Y |
+| Schild's Ladder | Greg Egan | 2026-02-26 | 2.5 | Y |
+| The Man Who Was Thursday | G.K. Chesterton | 2026-03-28 | 3.5 | Y |
+| Hunger | Knut Hamsun | 2026-03-31 | 4.5 | Y |
+| The Invention of Morel | Adolfo Bioy Casares | 2026-04-20 | 4 | Y |
+| The Invincible | Stanislaw Lem | 2026-04-26 | 3.5 | Y |
+`;export{n as default};
